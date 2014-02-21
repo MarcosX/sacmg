492b55961f4311dd043f8242e7888684b4dd75f6
Marcos Brizeno <marcos.uece.comp@gmail.com>
2014-02-21 11:25:31 -0300

    auto adding spec_helper file

--- spec/logic/piece/colored_spec.rb
+++ spec/logic/piece/colored_spec.rb
@@ -1,5 +1,3 @@
-require_relative "../../spec_helper"
-
 describe Piece::Colored do
   context "#initialize" do
     it "should have a color as type" do
--- spec/logic/piece/empty_spec.rb
+++ spec/logic/piece/empty_spec.rb
@@ -1,5 +1,3 @@
-require_relative "../../spec_helper"
-
 describe Piece::Empty do
   context "#initialize" do
     it "should create an nil object" do
