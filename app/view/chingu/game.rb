5de1f797eeb4995602d3f544a181acaffa5b9594
Marcos Brizeno <marcos.uece.comp@gmail.com>
2014-02-21 22:37:22 -0300

    adding extra logic to allow clicks on nearby pieces

--- app/view/chingu/game.rb
+++ app/view/chingu/game.rb
@@ -9,6 +9,7 @@ class Game < Chingu::Window
     super
     self.input = {escape: :exit, left_mouse_button: :click}
     @w = 60
+    @mouse_clicked = 0
     @grid = Grid.new(width: 10, height: 10)
     @grid.pieces.each_with_index do |row, i|
       row.each_with_index do |piece, j|
@@ -23,8 +24,17 @@ class Game < Chingu::Window
     cursor_y = $window.mouse_y.to_i/@w
     fill_rect [cursor_x*@w + @offset, cursor_y*@w, @w, @w], Gosu::Color::WHITE
 
-    if @mouse_clicked
+    if @mouse_clicked > 0
       fill_rect [@mouse_x*@w + @offset, @mouse_y*@w, @w, @w], Gosu::Color::WHITE
+      if @mouse_clicked > 1
+        @mouse_clicked = 0
+        if (@previous_mouse_x - @mouse_x).abs <= 1 && (@previous_mouse_y - @mouse_y).abs < 1 ||
+          (@previous_mouse_y - @mouse_y).abs <= 1 && (@previous_mouse_x - @mouse_x).abs < 1
+            @grid.play_move [@previous_mouse_y, @previous_mouse_x], [@mouse_y, @mouse_x]
+        else
+          @mouse_clicked = 1
+        end
+      end
     end
 
     @grid.each_piece do |piece|
@@ -36,9 +46,11 @@ class Game < Chingu::Window
   end
 
   def click
+    @previous_mouse_x = @mouse_x
+    @previous_mouse_y = @mouse_y
     @mouse_x = ($window.mouse_x.to_i-@offset)/@w
     @mouse_y = $window.mouse_y.to_i/@w
-    @mouse_clicked = true
+    @mouse_clicked += 1
   end
 
   protected
