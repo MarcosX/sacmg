c498b3df83dbbe620a873c6f943218466ad9dcf7
Marcos Brizeno <marcos.uece.comp@gmail.com>
2014-02-21 22:25:42 -0300

    adding cursor and click

--- app/view/chingu/game.rb
+++ app/view/chingu/game.rb
@@ -7,7 +7,8 @@ class Game < Chingu::Window
 
   def initialize
     super
-    self.input = {escape: :exit}
+    self.input = {escape: :exit, left_mouse_button: :click}
+    @w = 60
     @grid = Grid.new(width: 10, height: 10)
     @grid.pieces.each_with_index do |row, i|
       row.each_with_index do |piece, j|
@@ -17,14 +18,27 @@ class Game < Chingu::Window
   end
 
   def draw
-    @grid.each_piece do |piece|
-      w = 60
-      offset = ($window.width - @grid.width*w)/2
-      x = piece.x * w + offset
-      y = piece.y * w
-      color = map_piece_type(piece.type)
-      fill_rect [x+2, y+2, w-2, w-2], color
+    @offset = ($window.width - @grid.width*@w)/2
+    cursor_x = ($window.mouse_x.to_i-@offset)/@w
+    cursor_y = $window.mouse_y.to_i/@w
+    fill_rect [cursor_x*@w + @offset, cursor_y*@w, @w, @w], Gosu::Color::WHITE
+
+    if @mouse_clicked
+      fill_rect [@mouse_x*@w + @offset, @mouse_y*@w, @w, @w], Gosu::Color::WHITE
     end
+
+    @grid.each_piece do |piece|
+      x = piece.x * @w + @offset
+      y = piece.y * @w
+      color = map_piece_type(piece.type)
+      fill_rect [x+2, y+2, @w-4, @w-4], color
+    end
+  end
+
+  def click
+    @mouse_x = ($window.mouse_x.to_i-@offset)/@w
+    @mouse_y = $window.mouse_y.to_i/@w
+    @mouse_clicked = true
   end
 
   protected
