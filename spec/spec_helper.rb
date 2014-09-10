49dad0007288b352c39cda2163b0eebcada7c1b4
Marcos Brizeno <marcos.uece.comp@gmail.com>
2014-09-10 18:33:13 -0300

    updating color configuration for RSpec 3.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 Dir["app/logic/**/*.rb"].each {|file| require_relative "../#{file}" }
 
 RSpec.configure do |config|
-  # Ativa output colorido
-  config.color_enabled = true
   # Define qual o framework de mocks.
   # Pode ser :rspec, :mocha, :rr, :flexmock, :none
   config.mock_framework = :rspec
